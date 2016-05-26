64431479ef33a9be35203f09d0d4e1d5ccded438
Michael Arntzenius <daekharel@gmail.com>
2016-05-26 21:59:10 +0100

    whoo, it works

--- gen.py
+++ gen.py
@@ -3,31 +3,55 @@
 from collections import namedtuple
 import csv, random
 
+TWEET_LENGTH = 140
 FORMAT = "What if we combine {} and {}?"
 
-Idea = namedtuple('Idea', 'text url')
+# You can get this from twitter's API but that's a PITA, so we just hard-code
+# it. See https://dev.twitter.com/overview/t.co, "Will t.co-wrapped links always
+# be the same length?", and
+# https://dev.twitter.com/rest/reference/get/help/configuration.
+SHORT_URL_LENGTH_ESTIMATE = 24
+
+class Idea(namedtuple('Idea', 'what whose url')):
+    def short(self): return self.what
+    def long(self):
+        if self.whose: return self.whose + ' ' + self.what
+        else: return self.short()
 
 def read_ideas(filename="ideas.csv"):
     with open(filename) as f:
-        return [convert(line) for line in csv.reader(f)]
-        for line in csv.reader(f):
-            assert 1 <= len(line) <= 2
-            text = line[0]
-            url =  line[1] if len(line) > 1 else None
-            yield Idea(text, url)
+        # filter out whitespace lines and lines starting with "--"
+        lines = (l for l in f if l.strip() and not l.startswith('--'))
+        for line in csv.reader(lines, dialect='unix'):
+            assert 1 <= len(line) <= 3
+            what  = line[0].strip()
+            whose = line[1].strip() if len(line) > 1 else None
+            url   = line[2].strip() if len(line) > 2 else None
+            if not whose: whose = None
+            if not url: url = None
+            yield Idea(what, whose, url)
 
-def generate(all_ideas):
-    """Generates a random mash-up of ideas given a list of ideas."""
-    ideas = idea_a, idea_b = random.sample(all_ideas, 2)
-    text = FORMAT.format(idea_a.text, idea_b.text)
-    urls = [idea.url for idea in ideas if idea.url is not None]
+def mashup(a, b):
+    """Mashes up two ideas to make a tweet."""
+    # generate attributed & unattributed versions
+    short = FORMAT.format(a.short(), b.short())
+    long = FORMAT.format(a.long(), b.long())
+    # generate urls & calculate effective length
+    urls = [i.url for i in [a,b] if i.url]
+    urls_len = len(urls) * (SHORT_URL_LENGTH_ESTIMATE+1)
+    # pick long or short
+    text = long if len(long) + urls_len <= 140 else short
+    tweet = '\n'.join([text] + urls)
+    return (tweet, len(text) + urls_len)
 
 def run(filename="ideas.csv"):
-    ideas = list(read_ideas(filename))
+    all_ideas = list(read_ideas(filename))
     while True:
-        g = generate(ideas)
-        if g.length_ok(): break
-        else: print("Discarding: " ++ str(g))
-    return g
+        ideas = random.sample(all_ideas, 2)
+        (tweet, length) = mashup(*ideas)
+        if length <= TWEET_LENGTH: break
+        else: print("Discarding tweet, too long: " ++ tweet)
+    print(tweet)
 
-print("loaded")
+if __name__ == "__main__":
+    run()
